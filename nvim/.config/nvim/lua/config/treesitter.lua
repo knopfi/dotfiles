2bc88ec8bd2cc4f01c3a6da0bce4d45e93fda0b4
Peter Knoepfler <peter.knoepfler@gmail.com>
2025-11-23 12:18:44 +0100

    add additional treesitter parsers

---
+++ nvim/.config/nvim/lua/config/treesitter.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "nvm-treesitter/nvm-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "regex",
+        "typescript",
+        "vim",
+        "vue",
+        "yaml",
+      },
+    },
+  },
+}
